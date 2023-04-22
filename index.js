4f47ffb4395881f63111f8d1302ef67f4a7663f7
valeria caliguire <caliguireval@gmail.com>
2023-04-21 20:44:53 -0600

    finished

--- index.js
+++ index.js
@@ -11,6 +11,125 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+
+const titleCased = () =>  tutorials.map(sentence => {
+  const splitSentences = sentence.split(' ')
+  //declare new constant
+  const wordsCaps = splitSentences.map( word => word[0].toUpperCase() + word.slice(1))
+  //map through constant
+  //grab the 1st element in array, set to uppercase
+  //slice rest off of array
+  const completedCaps = wordsCaps.join(' ')
+  
+  return completedCaps
+})
+
+//console.log(titleCased)
+
+  // function capitalizeWords(arrayParam) {
+  //   return arrayParam.map((element) => {
+  //     const capitalizedFirst = element.charAt(0).toUpperCase();
+  //     const rest = element.slice(1).toLowerCase();
+  //     return capitalizedFirst + rest;  
+  //   });
+  
+
+//const wordsCaps = 
+// function capitalizeWords(arrayParam) {
+//   return arrayParam.map((element) => {
+//     const grabElementZero = element[0].split(" ")
+//     console.log(grabElementZero)
+//     // const capitalizedFirst = element.charAt(0).toUpperCase();
+//     // const rest = element.slice(1).toLowerCase();
+//     // return capitalizedFirst + rest;  
+//   });
+// }
+
+  //console.log(capitalizeWords(grabElementZero))
+
+
+
+//1. H"iterate" through individual words in a string?
+
+//access the first element of the array
+// splits where there are SPACES, "w" would split where there are "w"
+
+//const indivWords = tutorials[0].split(" ") // OPTION: const separateWords = indivWords.split(" ")
+//access the first element of the new array
+//const firstLetterCaps = indivWords[0][0].toUpperCase()
+
+
+ 
+// // const newArray = [...grabElementZero]
+// // console.log(newArray)
+
+// function capsWords(param) {
+//   for (let i = 1; i < tutorials.length; i++) {
+//     let eachSentence = tutorials[i].split(" ")
+//     //console.log(eachSentence)
+//   }
+//   console.log(capsWords(tutorials[1]))
+// }
+
+
+
+
+/******THIS WORKED****/
+// const wordsCaps = function capitalizeWords(arrayParam) {
+//   return arrayParam.map((element) => {
+//     const capitalizedFirst = element.charAt(0).toUpperCase();
+//     const rest = element.slice(1).toLowerCase();
+//     return capitalizedFirst + rest;  
+//   });
+//
+
+
+// const grabFirstWord = indivSentence[0]
+// const grabFirstLetter = grabFirstWord.split("")
+// const firstLetterCaps = grabFirstLetter.toUpperCase()
+
+
+
+
+
+// function makeCaps(param1) {
+//   let   indivSentence = param1[0]
+//   console.log(indivSentence)
+//     for (let i = 0; i < param1.length; i++) {
+//       //const indivWords = param1[i].split(" ")
+//       const grabFirstLetter  = indivSentence[i].split("") //.toUpperCase()
+        
+//      // const grabFirstLetter = indivWords.split("") 
+//       //firstLetterCaps
+      
+//       return grabFirstLetter
+      
+//     }
+    
+//   }
+  
+  //console.log(makeCaps(tutorials))
+
+//const separateWords = indivWords[0]
+// //make that element upper case
+// const separateLetters = separateWords[0].toUpperCase()
+
+
+
+
+
+
+/**************TRIED, DIDN'T WORK: NO HIERARCHY***************/
+// function individualizeWords(tutorialsParam) {
+//   let   sentence= tutorials[0]
+//   for (let i = 1; i < tutorialsParam.length; i++) {
+//     const indivWords = sentence[0]
+//     const currentWord= tutorialsParam[i]
+//     if (currentWord[i] ){
+
+//     }
+//   }
+// }
+
+
+
